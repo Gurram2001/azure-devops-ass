0e04b4c48061d35196ec60f64eeed64cfabab0f3
Mani Karthik Gurram <101256564+Gurram2001@users.noreply.github.com>
2025-04-15 19:40:49 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,59 @@
+variable "resource_group_name" {
+  type    = string
+  default = "modus-rg1"
+}
+
 variable "location" {
+  type    = string
   default = "Central US"
 }
 
-variable "prefix" {
-  description = "Prefix for naming resources"
-  default     = "devopsdemo"
+variable "app_service_plan_name" {
+  type    = string
+  default = "modus-asp"
 }
 
-variable "resource_group_name" {
-  default = "devopsdemo-rg"
+variable "app_service_name" {
+  type    = string
+  default = "modus-webapp"
 }
 
-variable "sql_admin" {
-  type = string
+variable "container_registry" {
+  type    = string
+  default = "<your_container_registry>"  # e.g., myregistry.azurecr.io
 }
 
-variable "sql_password" {
-  type      = string
-  sensitive = true
+variable "image_name" {
+  type    = string
+  default = "moduswebapi"
 }
 
+variable "sql_server_name" {
+  type    = string
+  default = "modus-sql-server"
+}
+
+variable "sql_admin_username" {
+  type    = string
+  default = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  type    = string
+  default = "P@ssword1234!" 
+}
+
+variable "sql_database_name" {
+  type    = string
+  default = "ModusDb"
+}
+
+variable "sql_sku" {
+  type    = string
+  default = "S0"
+}
+
+variable "key_vault_name" {
+  type    = string
+  default = "modus-kv-20251504"
+}
